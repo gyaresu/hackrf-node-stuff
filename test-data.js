2602cc3ab7533d8d37fd6bc5da075c300142b8a5
Gareth <me@gareth.codes>
2015-08-27 00:00:11 -0700

    hackrf-node-stuff - saving but broken

---
+++ test-data.js
@@ -0,0 +1,21 @@
+var devices = require('hackrf-stream')
+var fs = require('fs')
+
+var radio = devices(0).open(0)
+
+radio.setFrequency(9.15e8)
+
+radio.setSampleRate(3.7e6)
+
+var tx = radio.createWriteStream()
+
+fs.readFile('./gareth.raw', function (err, data) {
+  if (err) {
+    throw err
+  }
+  var img = data
+
+  while (true) {
+    tx.write(img)
+  }
+})
